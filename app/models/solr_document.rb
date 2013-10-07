d64c4dd6bc4e39bb805907be16771292be6bb2c5
Mike Korcynski <mkorcy@gmail.com>
2013-10-07 12:43:25 -0400

    generate different preview paths based on whether an object is or is not already published

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -4,19 +4,30 @@ class SolrDocument
   include Blacklight::Solr::Document
 
   # self.unique_key = 'id'
-  
+
   def published?
     self[Solrizer.solr_name("edited_at", :stored_sortable, type: :date)] == 
       self[Solrizer.solr_name("published_at", :stored_sortable, type: :date)]
   end
 
   def preview_fedora_path
-    Settings.preview_fedora_url + "/objects/#{id}" 
+    if published?
+      fedora_url = Settings.preview_fedora_prod_url
+    else
+      fedora_url = Settings.preview_fedora_stage_url
+    end
+
+    fedora_url + "/objects/#{id}" 
   end
-  
+
   def preview_dl_path
+    if published?
+      dl_url = Settings.preview_dl_prod_url
+    else
+      dl_url = Settings.preview_dl_stage_url
+    end
     if self['displays_ssi'].blank? || self['displays_ssi'] == 'dl'
-      Settings.preview_dl_url + "/catalog/#{id}" 
+      dl_url + "/catalog/#{id}" 
     else
       return nil
     end
