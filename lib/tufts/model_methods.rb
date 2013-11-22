2427c1229aeaf9cb66420190e58b49e8ab66c40b
Hydra Admin User <hydradm@hyd-dev-01.lib.tufts.edu>
2013-11-22 10:46:52 -0500

    commented some code we don't need for MIRA that we do use in TDL

--- lib/tufts/model_methods.rb
+++ lib/tufts/model_methods.rb
@@ -20,7 +20,7 @@ module Tufts
     def create_facets(solr_doc)
       index_names_info(solr_doc)
       index_subject_info(solr_doc)
-      index_collection_info(solr_doc)
+      #index_collection_info(solr_doc)
       index_date_info(solr_doc)
       index_format_info(solr_doc)
       index_pub_date(solr_doc)
