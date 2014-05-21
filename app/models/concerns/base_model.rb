81dc83cf86ce812d03aac197af4d76e75c5bdcd0
Mike Korcynski <mkorcy@gmail.com>
2014-05-21 14:50:30 -0400

    resolves code mismerged

--- app/models/concerns/base_model.rb
+++ app/models/concerns/base_model.rb
@@ -4,7 +4,8 @@ module BaseModel
   included do
     include Tufts::ModelMethods
     include Hydra::AccessControls::Permissions
-    include AttachedFiles
+
+    validate :relationships_have_parseable_uris
 
     # Uses the Hydra Rights Metadata Schema for tracking access permissions & copyright
     has_metadata "rightsMetadata", type: Hydra::Datastream::RightsMetadata
@@ -24,19 +25,27 @@ module BaseModel
       self.edited_at = DateTime.now
       self.admin.published_at = edited_at if push_production
 
-      if content_will_update 
-        self.audit(working_user, "Content updated: #{content_will_update}")
-        self.content_will_update = nil
-      elsif metadata_streams.any? { |ds| ds.changed? }
-        self.audit(working_user, "Metadata updated #{metadata_streams.select { |ds| ds.changed? }.map{ |ds| ds.dsid}.join(', ')}")
-      end
-
       # Don't change existing OAI IDs, but for any objects with a display portal of 'dl', generate an OAI ID
       if displays.include?('dl') && !object_relations.has_predicate?(:oai_item_id)
         self.add_relationship(:oai_item_id, "oai:#{pid}", true)
         # we didn't use .serialize! here because it would mark the model as clean and then
         # never actually save to Fedora
         self.rels_ext.content = rels_ext.to_rels_ext()
+      end
+    end
+
+    before_save :update_audit_log
+
+    def update_audit_log
+      if respond_to?(:content_will_update) && content_will_update
+        self.audit(working_user, "Content updated: #{content_will_update}")
+        self.content_will_update = nil
+      elsif metadata_streams.any? { |ds| ds.changed? }
+        self.audit(working_user, "Metadata updated #{metadata_streams.select { |ds| ds.changed? }.map{ |ds| ds.dsid}.join(', ')}")
+      end
+
+      if push_production
+        self.audit(working_user, 'Pushed to production')
       end
     end
 
@@ -91,6 +100,89 @@ module BaseModel
     self.rels_ext.content = rels_ext.to_rels_ext()
   end
 
+  def relationship_attributes
+    rels = []
+    rels_ext_edit_fields.each do |rel|
+      predicate = object_relations.uri_predicate(rel)
+      uris = object_relations.relationships[predicate]
+      unless uris.empty?
+        rel_pids = uris.map {|uri| uri.gsub("info:fedora/", "")}
+        rel_pids.each do |pid|
+          builder = RelationshipBuilder.new(rel, pid)
+          rels << builder
+        end
+      end
+    end
+    # Include invalid relationships for displaying errors on the edit form
+    rels = rels + @invalid_rels unless @invalid_rels.blank?
+    rels
+  end
+
+  # Set rels-ext according to what the user entered on the edit form
+  def relationship_attributes=(attrs)
+
+    # Don't try to set relationships that fail validation
+    validate_relationships(attrs)
+    @invalid_rels.each do |rel|
+      attrs = attrs.reject{|a| a['relationship_name'] == rel.relationship_name && a['relationship_value'] == rel.relationship_value }
+    end
+
+    # Clear out old relationships
+    rels_ext_edit_fields.each do |predicate_name|
+      pred = ActiveFedora::Predicates.find_graph_predicate(predicate_name)
+      clear_relationship(pred)
+    end
+
+    attrs = attrs.reject{|e| e['relationship_name'].blank? || e['relationship_value'].blank? }
+
+    # Only accept fields that users are allowed to edit
+    attrs = attrs.reject{|e| !rels_ext_edit_fields.include?(e['relationship_name'].to_sym) }
+
+    attrs.each do |attr|
+      predicate_name = attr['relationship_name'].to_sym
+      pred = ActiveFedora::Predicates.find_graph_predicate(predicate_name)
+      add_relationship(pred, 'info:fedora/' + attr['relationship_value'])
+    end
+    self.rels_ext.content = rels_ext.to_rels_ext()
+  end
+
+  # The way validation normally works, the value of an
+  # attribute is set first, and then it is validated.  But we
+  # have a case where we can't set the value first because
+  # setting the value raises an error.
+  # (When there is a PID that won't parse into a URI, calling
+  # relationship_attributes= raises URI::InvalidURIError)
+  # Later when validation is called, all errors are cleared
+  # at the beginning of validation, so the URI::InvalidURIError
+  # messages are lost.
+  #
+  # This is a kludge to keep track of those invalid inputs
+  # and their corresponding error messages so that they can
+  # be displayed to the user on the edit form.
+  #
+  def validate_relationships(attrs)
+    @invalid_rels = []
+    @invalid_rels_errors = []
+
+    attrs.each do |rel|
+      begin
+        URI.parse rel['relationship_value']
+      rescue URI::InvalidURIError
+        @invalid_rels << RelationshipBuilder.new(rel['relationship_name'], rel['relationship_value'])
+        @invalid_rels_errors << "Invalid relationship: \"#{rel['relationship_name'].to_s.titleize}\" : \"#{rel['relationship_value']}\""
+      end
+    end
+  end
+  protected :validate_relationships
+
+  def relationships_have_parseable_uris
+    @invalid_rels_errors ||= []
+    @invalid_rels_errors.each do |err|
+      errors.add(:base, err)
+    end
+  end
+  protected :relationships_have_parseable_uris
+
   def audit(user, what)
     return unless user
     audit_log.who = user.user_key
@@ -118,6 +210,20 @@ module BaseModel
     descMetadata_display_fields + admin_display_fields
   end
 
+  def rels_ext_edit_fields
+    [:has_equivalent,
+     :is_annotation_of, :has_annotation,
+     :is_constituent_of, :has_constituent,
+     :is_dependent_of, :has_dependent,
+     :is_derivation_of, :has_derivation,
+     :is_description_of, :has_description,
+     :is_member_of, :has_member,
+     :is_member_of_collection, :has_collection_member,
+     :is_metadata_for, :has_metadata,
+     :is_part_of, :has_part,
+     :is_subset_of, :has_subset]
+  end
+
   # The list of fields to edit from the DCA_META datastream
   def descMetadata_display_fields
     [:identifier, :title, :alternative, :creator, :contributor, :description, :abstract,
@@ -133,7 +239,7 @@ module BaseModel
 
   # The list of fields to edit from the DCA_ADMIN datastream
   def admin_display_fields
-    admin.class.terminology.terms.keys  - [:admin, :published_at, :edited_at]
+    admin.class.terminology.terms.keys  - [:admin, :published_at, :edited_at, :template_name, :batch_id]
   end
 
   # a more idiomatic name for the DC-DETAIL-META datastream
@@ -168,9 +274,16 @@ module BaseModel
   end
 
   # Publish the record to the production fedora server
+  def publish!(user_id = nil)
+    self.working_user = User.where(id: user_id).first
+    push_to_production!
+  end
+
   def push_to_production!
     self.push_production = true
-    if save
+    save_succeeded = save
+    self.push_production = false
+    if save_succeeded
       # Now copy to prod
       # Rubydora::FedoraInvalidRequest
       foxml = self.inner_object.repository.api.export(pid: pid, context: 'archive')
@@ -183,26 +296,33 @@ module BaseModel
     end
   end
 
+  def purge!
+    production_fedora_connection.purge_object(pid: pid) rescue RestClient::ResourceNotFound
+    update_attributes(state: "D")
+  end
+
   def production_fedora_connection
     @prod_repo ||= Rubydora.connect(ActiveFedora.data_production_credentials)
   end
 
   def to_solr(solr_doc=Hash.new, opts={})
-     if pid[/perseus/]
-       solr_doc = {id: pid,object_type_sim: "Perseus Images",title_tesim: title,active_fedora_model_ssi: "TuftsImage",has_model_ssim: "info:fedora/cm:Image.4DS"}
-       solrize_profile(solr_doc)
-     else
-         begin
-           solr_doc = super
-         rescue
-           logger.error("ERROR while indexing #{pid} will attempt to recover but object may be incomplete in index")
-           solrize_profile(solr_doc)
-         end
-       create_facets solr_doc
-       index_sort_fields solr_doc
-     end
-    solr_doc
-  end
+
+      if pid[/perseus/]
+        solr_doc = {id: pid,object_type_sim: "Perseus Images",title_tesim: title,active_fedora_model_ssi: "TuftsImage",has_model_ssim: "info:fedora/cm:Image.4DS"}
+        solrize_profile(solr_doc)
+      else
+        begin
+          solr_doc = super
+        rescue
+          logger.error("ERROR while indexing #{pid} will attempt to recover but object may be incomplete in index")
+          solrize_profile(solr_doc)
+        end
+        create_facets solr_doc
+        index_sort_fields solr_doc
+      end
+      solr_doc
+    end
+
 
   # override this method if you want to restrict the accepted formats to a particular mime-type
   # @param [String] dsid Datastream id
@@ -211,28 +331,20 @@ module BaseModel
     true
   end
 
-  def apply_attributes(attributes, user_id = nil)
-    # stored_collection_id is a special case because it's
-    # not a defined attribute in active-fedora; it's a
-    # derived attribute.
-    attrs_for_update = { stored_collection_id: attributes.delete(:stored_collection_id) }
-
-    # For attributes that can have multiple values, we want to
-    # add the new value to the existing values, not overwrite
-    # the existing values.
-    attributes.each do |key, value|
-      if self.class.multiple?(key)
-        attrs_for_update[key] = (self.send(key) + Array(value)).uniq
-      else
-        attrs_for_update[key] = value
-      end
-    end
-
-    self.working_user = User.where(id: user_id).first
-    update_attributes(attrs_for_update)
-  end
-
   def has_thumbnail?
     false
+  end
+
+  module ClassMethods
+    def revert_to_production(pid)
+      prod = Rubydora.connect(ActiveFedora.data_production_credentials)
+      begin
+        foxml = prod.api.export(pid: pid, context: 'archive')
+      rescue RestClient::ResourceNotFound
+        raise ActiveFedora::ObjectNotFoundError.new("Could not find pid #{pid} on production server")
+      end
+      connection_for_pid(pid).purge_object(pid: pid) rescue RestClient::ResourceNotFound
+      connection_for_pid(pid).ingest(file: foxml)
+    end
   end
 end
