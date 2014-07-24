85c7200f15fa7fc128575cce368ee64bcfc9cc60
Mike Korcynski <mkorcy@gmail.com>
2014-07-24 09:24:01 -0400

    change for displayname in test against non-prod ldap
    Conflicts:
    	app/models/user.rb
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20150626175634) do
     t.boolean  "guest",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
